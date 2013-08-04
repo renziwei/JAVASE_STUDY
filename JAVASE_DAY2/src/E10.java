af5334cf9af9a0b8f28d7a60eff6ae30c546efc1
renziwei <61909786@qq.com>
2013-08-04 17:03:46 +0800

    sd

---
+++ JAVASE_DAY2/src/E10.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 13-8-4.
+ */
+//【程序10】题目：一球从100米高度自由落下，每次落地后反跳回原高
+// 度的一半；再落下，求它在第10次落地时，共经过多少米？第10次反
+// 弹多高？
+public class E10{
+    public static void main(String[] args) {
+        int m=100;
+        int n;
+        for(n=1,n<=10,n++){
+
+        }
+    }
+}
