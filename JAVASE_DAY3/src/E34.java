51a7886b43a75f8076656f197c928a068f3fa418
renziwei <61909786@qq.com>
2013-08-10 16:59:15 +0800

    sd

---
+++ JAVASE_DAY3/src/E34.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 13-8-10.
+ */
+public class E34 {
+    public static void main(String[] args) {
+        for (int i=0;i<10000;i++) {
+            for(int j=1;j<20;j++){
+                System.out.println(j);
+            }
+        }
+
+    }
+
+
+}
