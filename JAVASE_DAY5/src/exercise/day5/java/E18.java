874d20174922a0a0774dbcc0b512e1be487d3c32
renziwei <61909786@qq.com>
2013-08-17 16:09:43 +0800

    sd

--- JAVASE_DAY5/src/exercise/day5/java/E18.java
+++ JAVASE_DAY5/src/exercise/day5/java/E18.java
@@ -5,4 +5,8 @@ package exercise.day5.java;
  */
 //【程序18】题目：两个乒乓球队进行比赛，各出三人。甲队为a,b,c三人，乙队为x,y,z三人。已抽签决定比赛名单。有人向队员打听比赛的名单。a说他不和x比，c说他不和x,z比，请编程序找出三队赛手的名单。
 public class E18 {
+    public static void main(String[] args) {
+        String a,b,c,x,y,z;
+
+    }
 }
